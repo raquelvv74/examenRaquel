e49574c8ca9292ce11a810b197385bc0d06bda60
Raquel <raquelvv74@educastur.es>
2024-02-23 19:07:21 +0100

    Añadidos los primeros ficheros al proyecto

---
+++ src/miExamen/ClaseExamen.java
@@ -0,0 +1,18 @@
+
+package miExamen;
+
+/**
+ *
+ * @author alu28v
+ */
+public class ClaseExamen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Estoy haciendo el examen de la UT3");
+    }
+    
+}
